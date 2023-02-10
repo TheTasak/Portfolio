2984d03ded3043d162cec42922b1ef6e5dad7b03
Przemysław Tasarz <przemek433@interia.eu>
2023-02-10 16:49:18 +0100

    scroll observer

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <div className="about" id="about">
             <div className="content-center">
-                <div className="text-main">Przemysław Tasarz</div>
-                <div className="text-sub">Full-Stack Developer</div>
+                <div className="text-main hidden">Przemysław Tasarz</div>
+                <div className="text-sub hidden">Full-Stack Developer</div>
             </div>
         </div>
     )
