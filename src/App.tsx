6162d0c210c4f1c325834028c25d3786b1e9abdd
Przemysław Tasarz <przemek433@interia.eu>
2023-02-27 17:37:53 +0100

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Experience from './components/Experience/Experience';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
       <About />
       <Experience />
       <Projects />
+      <Footer />
     </div>
   )
 }
