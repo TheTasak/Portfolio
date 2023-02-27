0bf650552aaea140b0f5185d805a63b258498191
Przemysław Tasarz <przemek433@interia.eu>
2023-02-27 17:52:13 +0100

    fix photo url v2

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import './Projects.scss';
 import TextHeader from '../Utilities/TextHeader';
 import ProjectCard from './ProjectCard';
+import imgUrl from '../../assets/project1.png';
 
 const projectData = 
 [
@@ -8,7 +9,7 @@ const projectData =
         id: 1,
         title: 'Bookemia',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique blandit libero sed pharetra. Maecenas blandit dolor id sapien cursus, a tristique lectus maximus. Integer porta molestie magna, at rhoncus erat. Nunc condimentum, magna quis interdum iaculis, libero ex consectetur ante, quis mollis diam justo nec neque. Integer quam est, laoreet et ornare et, maximus non leo. Nulla eu nulla ac metus tempus cursus. Suspendisse nec erat pharetra, rutrum nibh non, fermentum est.',
-        photoUrl: '/src/assets/project1.png',
+        photoUrl: imgUrl,
         githubUrl: 'https://github.com/TheTasak/Bookemia',
         demoUrl: '',
         technologies: [
@@ -19,7 +20,7 @@ const projectData =
         id: 2,
         title: 'BossaTransactionVisualiser',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique blandit libero sed pharetra. Maecenas blandit dolor id sapien cursus, a tristique lectus maximus. Integer porta molestie magna, at rhoncus erat. Nunc condimentum, magna quis interdum iaculis, libero ex consectetur ante, quis mollis diam justo nec neque. Integer quam est, laoreet et ornare et, maximus non leo. Nulla eu nulla ac metus tempus cursus. Suspendisse nec erat pharetra, rutrum nibh non, fermentum est.',
-        photoUrl: '/src/assets/project1.png',
+        photoUrl: imgUrl,
         githubUrl: 'https://github.com/TheTasak/Bookemia',
         demoUrl: '',
         technologies: [
@@ -30,7 +31,7 @@ const projectData =
         id: 3,
         title: 'EmergingPoland',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique blandit libero sed pharetra. Maecenas blandit dolor id sapien cursus, a tristique lectus maximus. Integer porta molestie magna, at rhoncus erat. Nunc condimentum, magna quis interdum iaculis, libero ex consectetur ante, quis mollis diam justo nec neque. Integer quam est, laoreet et ornare et, maximus non leo. Nulla eu nulla ac metus tempus cursus. Suspendisse nec erat pharetra, rutrum nibh non, fermentum est.',
-        photoUrl: '/src/assets/project1.png',
+        photoUrl: imgUrl,
         githubUrl: 'https://github.com/TheTasak/Bookemia',
         demoUrl: '',
         technologies: [
